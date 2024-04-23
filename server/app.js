fe270f7e4ee07a184eb61dfd3350bc5e0d374e9d
Marcus Holje <99866281+norskemarcus@users.noreply.github.com>
2024-04-23 19:42:31 +0200

    Added models and some routers

---
+++ server/app.js
@@ -0,0 +1,23 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+import usersRouter from './routers/usersRouter.js';
+app.use(usersRouter);
+
+import schoolsRouter from './routers/schoolsRouter.js';
+app.use(schoolsRouter);
+
+import locationsRouter from './routers/locationsRouter.js';
+app.use(locationsRouter);
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, error => {
+  if (error) {
+    console.log('Server failed to start', error);
+    return;
+  }
+  console.log('Server is running on port', PORT);
+});
---
+++ server/routers/locationsRouter.js
@@ -0,0 +1,31 @@
+import Router from 'express';
+const router = Router();
+import Location from '../database/models/location.js';
+
+router.get('/api/locations', async (req, res) => {
+  const locations = await Location.findAll();
+  console.log(locations);
+  res.send({ data: locations });
+});
+
+router.post('/api/locations', async (req, res) => {
+  const { school_id, zip_code, city, street_name, street_number } = req.body.data;
+
+  try {
+    const new_location = await Location.create({
+      school_id: school_id,
+      zip_code: zip_code,
+      city: city,
+      street_name: street_name,
+      street_number: street_number,
+    });
+
+    console.log(`${new_location.city} ${new_location.zip_code}`);
+    res.status(200).send({ data: new_location });
+  } catch (error) {
+    console.error('Error creating the location:', error);
+    res.status(500).send({ error: 'Failed to create location' });
+  }
+});
+
+export default router;
