abb96735a407c3213b1f142219293231c441f01e
Tommyk <tomm8231@stud.kea.dk>
2024-04-26 12:04:59 +0200

    Database create hot fix

--- server/database/database.js
+++ server/database/database.js
@@ -1,28 +1,16 @@
 import 'dotenv/config';
 import { Sequelize } from 'sequelize';
 
-let sequelize = new Sequelize(null, process.env.DB_USER, process.env.DB_PASS, {
+const connection = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
   host: 'localhost',
   dialect: 'mysql',
 });
 
 try {
-  await sequelize.authenticate();
+  await connection.authenticate();
   console.log('Connection has been established successfully.');
-  await sequelize.query(`DROP DATABASE IF EXISTS ${process.env.DB_NAME}`)
-  await sequelize.query(`CREATE DATABASE ${process.env.DB_NAME}`)
-  console.log('DATABASE DROPPED AND CREATED');
-
-  sequelize.close()
-
-  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-    host: 'localhost',
-    dialect: 'mysql',
-  });
-  
-  await sequelize.authenticate();
 } catch (error) {
   console.error('Unable to connect to the database:', error);
 }
 
-export default sequelize;
+export default connection;
