31fce1bb95637203b21f143409b78b7ffbd3fdd8
Tommyk <tomm8231@stud.kea.dk>
2024-05-07 15:35:20 +0200

    fixed login/logout bug when refreshing the browser window

--- client/src/routes/+layout.svelte
+++ client/src/routes/+layout.svelte
@@ -7,30 +7,38 @@
 	import { user } from '../stores/userStore.js';
 	import toast from 'svelte-french-toast';
 	import { onMount } from 'svelte';
+	import { goto } from '$app/navigation';
 
-	onMount(() => {
+	let isLoading = true;
+
+	onMount(async () => {
+
 		if (localStorage.getItem('sid')) {
+			try {
+				const sid = localStorage.getItem('sid');
+				const response = await fetch(`${$AUTH_URL}/auth/validateSession`, {
+					method: 'POST',
+					credentials: 'include',
+					headers: {
+						'Content-Type': 'application/json'
+					},
+					body: JSON.stringify({ sid })
+				});
 
-			async function validateSession() {
-			const sid = localStorage.getItem('sid');
-			const response = await fetch(`${$AUTH_URL}/auth/validateSession`, {
-				credentials: 'include',
-				headers: {
-					'Content-Type': 'application/json',
-				},
-				body: JSON.stringify({ sid })
-			});
-
-			if (response.ok) {
-				const result = await response.json();
-				user.set(result.data);
-			} else {
-				localStorage.removeItem('sid');
+				if (response.ok) {
+					const result = await response.json();
+					user.set(result.data);
+					goto('/'); // Should navigate back to the previous page
+				} else {
+					localStorage.removeItem('sid');
+				}
+			} catch (error) {
+				console.error(error);
+			} finally {
+				isLoading = false;
 			}
-		}
-		
-			validateSession();
-
+		} else {
+			isLoading = false;
 		}
 	});
 
@@ -51,7 +59,9 @@
 				credentials: 'include'
 			});
 
-			if (!response.ok) {
+			if (response.ok) {
+				goto('/');
+			} else {
 				toast.error('Fejl ved logud, kontakt admin');
 			}
 		} catch (error) {
@@ -74,11 +84,12 @@
 			if (response.ok) {
 				const result = await response.json();
 				user.set(result.data);
-				localStorage.setItem('sid', result.session)
+				localStorage.setItem('sid', result.session);
 				console.log($user);
 
 				newUser.email = '';
 				newUser.password = '';
+				goto('/');
 			}
 		} catch (error) {
 			console.error('Error login:', error);
@@ -87,87 +98,97 @@
 	}
 </script>
 
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-	<div class="container-fluid">
-		<a class="navbar-brand" href="/">Aftenskolerne</a>
-		<button
-			on:click={toggleOpen}
-			class="navbar-toggler"
-			type="button"
-			data-bs-toggle="collapse"
-			data-bs-target="#navbarNavAltMarkup"
-			aria-controls="navbarNavAltMarkup"
-			aria-expanded="false"
-			aria-label="Toggle navigation"
-		>
-			<span class="navbar-toggler-icon"></span>
-		</button>
-		<div class:show={isOpen} class="collapse navbar-collapse" id="navbarNavAltMarkup">
-			<div class="navbar-nav">
-				<a
-					class="nav-link"
-					aria-current="page"
-					class:active={$page.url.pathname === '/booking'}
-					data-sveltekit-preload-data
-					href="/booking">Booking</a
-				>
+{#if !isLoading}
+	<nav class="navbar navbar-expand-lg bg-body-tertiary">
+		<div class="container-fluid">
+			<a class="navbar-brand" href="/">Aftenskolerne</a>
+			<button
+				on:click={toggleOpen}
+				class="navbar-toggler"
+				type="button"
+				data-bs-toggle="collapse"
+				data-bs-target="#navbarNavAltMarkup"
+				aria-controls="navbarNavAltMarkup"
+				aria-expanded="false"
+				aria-label="Toggle navigation"
+			>
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class:show={isOpen} class="collapse navbar-collapse" id="navbarNavAltMarkup">
+				<div class="navbar-nav">
+					<a
+						class="nav-link"
+						aria-current="page"
+						class:active={$page.url.pathname === '/booking'}
+						data-sveltekit-preload-data
+						href="/booking">Booking</a
+					>
 
-				<a
-					class="nav-link"
-					class:active={$page.url.pathname === '/event'}
-					data-sveltekit-preload-data
-					href="/event">Event</a
-				>
-				<a
-					class="nav-link"
-					class:active={$page.url.pathname === '/courses'}
-					data-sveltekit-preload-data
-					href="/courses">Hold</a
-				>
+					<a
+						class="nav-link"
+						class:active={$page.url.pathname === '/event'}
+						data-sveltekit-preload-data
+						href="/event">Event</a
+					>
+					<a
+						class="nav-link"
+						class:active={$page.url.pathname === '/courses'}
+						data-sveltekit-preload-data
+						href="/courses">Hold</a
+					>
 
-				<a
-					class="nav-link"
-					class:active={$page.url.pathname === '/users'}
-					data-sveltekit-preload-data
-					href="/users">Kontoransatte</a
-				>
-				<a
-					class="nav-link"
-					class:active={$page.url.pathname === '/afdelinger'}
-					data-sveltekit-preload-data
-					href="/location">Afdelinger</a
-				>
+					<a
+						class="nav-link"
+						class:active={$page.url.pathname === '/users'}
+						data-sveltekit-preload-data
+						href="/users">Kontoransatte</a
+					>
+					<a
+						class="nav-link"
+						class:active={$page.url.pathname === '/afdelinger'}
+						data-sveltekit-preload-data
+						href="/location">Afdelinger</a
+					>
+				</div>
+
+				{#if !$user.email}
+					<form on:submit|preventDefault={login} class="d-flex ms-auto">
+						<input
+							class="form-control me-2"
+							type="text"
+							placeholder="Email"
+							aria-label="Email"
+							bind:value={newUser.email}
+						/>
+						<input
+							class="form-control me-2"
+							type="password"
+							placeholder="Password"
+							aria-label="Password"
+							bind:value={newUser.password}
+						/>
+						<button class="btn btn-outline-success" type="submit">Login</button>
+					</form>
+				{:else}
+					<button class="btn btn-outline-success d-flex ms-auto" on:click={logout}>Logud</button>
+				{/if}
 			</div>
-
-			{#if !$user.email}
-				<form on:submit|preventDefault={login} class="d-flex ms-auto">
-					<input
-						class="form-control me-2"
-						type="text"
-						placeholder="Email"
-						aria-label="Email"
-						bind:value={newUser.email}
-					/>
-					<input
-						class="form-control me-2"
-						type="password"
-						placeholder="Password"
-						aria-label="Password"
-						bind:value={newUser.password}
-					/>
-					<button class="btn btn-outline-success" type="submit">Login</button>
-				</form>
-			{:else}
-				<button class="btn btn-outline-success d-flex ms-auto" on:click={logout}>Logud</button>
-			{/if}
 		</div>
-	</div>
-</nav>
+	</nav>
 
-<main class="container">
-	<!-- TODO: conditional logik her -->
-	<slot />
-</main>
+	<main class="container">
+		<!-- TODO: conditional logik her -->
+		<slot />
+	</main>
+{:else}
+	<!-- Show spinner while loading -->
+	<div
+		class="d-flex justify-content-center align-items-center"
+		style="position: fixed; top: 0; right: 0; bottom: 0; left: 0; background-color: rgba(255, 255, 255, 0.5);"
+	>
+		<div class="spinner-border text-primary" role="status"></div>
+	</div>
+{/if}
 
 <style>
 	.nav-container {
